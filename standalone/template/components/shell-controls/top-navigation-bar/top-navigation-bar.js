20d32f9d13ae7894983fb51e31076b4cf0664c9c
REDMOND\v-jagahe <v-jagahe@microsoft.com>
2018-06-06 14:46:15 -0700

    clean up & prep for more top nav icons

--- standalone/template/components/shell-controls/top-navigation-bar/top-navigation-bar.js
+++ standalone/template/components/shell-controls/top-navigation-bar/top-navigation-bar.js
@@ -46,19 +46,26 @@ class TopNavigationBar extends React.Component
         //        React.createElement(ButtonCtrl)
         //    );
         //}
+
         function createMarkup(svgIcon) {
             return { __html: svgIcon };
         };
 
+        let _plus = SVG.Add.SVG;
+
+        //button
         let _a = React.createElement(ButtonCtrl, { className: 'Jackie', Application: this.props }); //(ButtonCtrl, {}, undefined) 
+
+        //App title (Azure)
         let _b = React.createElement('div', { id: this.ID, className: 'appTitle' }, `${this.Title}`);
 
-        let _svg = SVG.Add.SVG;
-        let _d = React.createElement('div', { className: 'icon', dangerouslySetInnerHTML: createMarkup(_svg) } );
+        //icon
+        let _c = React.createElement('div', { className: 'icon', dangerouslySetInnerHTML: createMarkup(_plus) } );
 
-        let _c = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, _b, _a, _d);
+        //topnav container
+        let _d = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, _b, _a, _c);
 
-        return _c;
+        return _d;
 
 	};
 };
