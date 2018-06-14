694f5faf1467d3e7061ff405fbe9255114ccc257
REDMOND\v-jagahe <v-jagahe@microsoft.com>
2018-06-13 17:06:44 -0700

    vert nav extensions

--- standalone/template/components/shell-controls/vertical-navigation-bar/vertical-navigation-bar.js
+++ standalone/template/components/shell-controls/vertical-navigation-bar/vertical-navigation-bar.js
@@ -22,7 +22,32 @@ class VerticalNavigationBar extends React.Component
 		this.state = {
 			IsDirty: false,
 			CurrentCssClass: this.CssClassNames.Normal
-		};
+        };
+
+        //individual extensions 
+        //let _a = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, );
+        let _a = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'New', buttonText: 'New', Application: this.props, svgIcon: SVG.Color.NewPlusSign, key: Utilities.NewId() });
+        let _b = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Dashboard', buttonText: 'Dashboard', Application: this.props, svgIcon: SVG.Color.Dashboard, key: Utilities.NewId() });
+        let _c = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Resource groups', buttonText: 'Resource groups', Application: this.props, svgIcon: SVG.Color.ResourceGroup, key: Utilities.NewId() });
+        let _d = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'App Services', buttonText: 'App Services', Application: this.props, svgIcon: SVG.Color.Websites, key: Utilities.NewId() });
+        let _e = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Function Apps', buttonText: 'Function Apps', Application: this.props, svgIcon: SVG.Color.Functions, key: Utilities.NewId() });
+        let _f = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Databases', buttonText: 'Databases', Application: this.props, svgIcon: SVG.Color.SQLDatabases, key: Utilities.NewId() });
+        let _g = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Azure Cosmos DB', buttonText: 'Azure Cosmos DB', Application: this.props, svgIcon: SVG.Color.CosmosDB, key: Utilities.NewId() });
+        let _h = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Virtual Machines', buttonText: 'Virtual Machines', Application: this.props, svgIcon: SVG.Color.VirtualMachines, key: Utilities.NewId() });
+        let _i = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Load Balancer', buttonText: 'Load Balancer', Application: this.props, svgIcon: SVG.Color.LoadBalancer, key: Utilities.NewId() });
+        let _j = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Storage Account', buttonText: 'Storage Account', Application: this.props, svgIcon: SVG.Color.Storage, key: Utilities.NewId() });
+        let _k = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Virtual Network', buttonText: 'Virtual Network', Application: this.props, svgIcon: SVG.Color.VirtualNetworks, key: Utilities.NewId() });
+        let _l = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Azure Active Directory', buttonText: 'Azure Active Directory', Application: this.props, svgIcon: SVG.Color.ActiveDirectory, key: Utilities.NewId() });
+        let _m = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Advisor', buttonText: 'Advisor', Application: this.props, svgIcon: SVG.Color.Advisor, key: Utilities.NewId() });
+        let _n = React.createElement(ButtonCtrl, { className: 'extension', attributeTitle: 'Security Center', buttonText: 'Security Center', Application: this.props, svgIcon: SVG.Color.SecurityCenter, key: Utilities.NewId() });
+
+        let extensionsArray = [_a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l, _m, _n];
+        //left nav container for all extensions 
+        this.vertnavbar = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, extensionsArray ); // `${this.Title}`
+
+
+
+
 
 		if ( this.props.Application.props.VerticalNavigationBar.Title !== undefined )
 		{
@@ -39,17 +64,8 @@ class VerticalNavigationBar extends React.Component
 	{	//	console.debug( "VerticalNavigationBar.render()", this.props );
 		Utilities.InjectControlCss( this.CssFileID, this.CssFile );
 
-		//return React.createElement( 'div', {
-		//	id: this.ID,
-		//	className: this.state.CurrentCssClass
-  //       }, `${this.Title}`);
-    
+        //let _d = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, ); // `${this.Title}`
 
-        let _a = React.createElement(ButtonCtrl, { className: 'Jackie', Application: this.props }); //(ButtonCtrl, {}, undefined) 
-        let _b = React.createElement('div', { id: this.ID, className: 'appTitle' }, `${this.Title}`);
-        let _c = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass, }, _a, _b); //dangerouslySetInnerHTML: this.createMarkup(SVG.Person.SVG)
-        let _d = React.createElement('div', { id: this.ID, className: this.state.CurrentCssClass }, ); // `${this.Title}`
-
-        return _d;
+        return this.vertnavbar;
 	};
 };
