cbd37edf7d2d3e18714cf786d455114e740f87ad
Jackie <jagaheri@microsoft.com>
2019-04-04 13:54:39 -0700

    button experimentation

---
+++ node/template/acuity-node/src/components/button/button2.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+// import { Utilities } from './../../js/utilities';
+// import SvgIcon from "./../svg-icons/svg-icon";
+
+export default class Button extends Component
+{
+	constructor( props )
+	{
+		super( props );
+
+		//properties
+		this.Id = this.props.id;
+		//	this.Object = this.props.objectType;
+        this.Application = this.props.Application;
+        this.Title = this.props.buttonText || "Button";
+		this.SvgIcon = this.props.svgIcon || undefined;
+        this.ButtonStyle = this.props.className;
+        this.CssFileID = "btn-css";
+        this.CssFile = "components/standard-controls/button/button.css";
+		this.CssClassNames = {
+            Normal: this.Theme + " buttonControl " + this.ButtonStyle,
+            Alerted: this.Theme + " RootLinkCssExtra " + this.ButtonStyle,
+		};
+
+		// state
+		this.state = {
+			items: [],
+			inner_text: this.props.InnerText,
+			prev_text: this.props.InnerText,
+			isClicked: false,
+            currentCssClass: this.CssClassNames.Normal,
+			AltTextTitle: this.props.attributeTitle,
+			IsLeftNavCollapsed: (this.props.IsCollapsed || false)
+        };
+
+
+		return;
+	 }
+	render()
+	{
+		return (
+            <div className={this.props.btn_class}>{this.props.title}</div>
+            // <div id="btn-text" onClick={this.HandleClick} title={this.state.AltTextTitle} >{this.Title}</div>
+		);
+	}
+}
